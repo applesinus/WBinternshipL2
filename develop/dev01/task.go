ce8757e36a828b154420fa7e1e53d65c7723e044
applesinus PC <dmitryapelsin4ik@gmail.com>
2024-03-24 21:58:58 +0300

    [ADD] task 1 + test

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -12,6 +12,19 @@ package main
 Программа должна проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while getting time from ntp: %v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("Current time: %v\n", time)
+	}
 }
--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func TestMain(t *testing.T) {
+	response, err := ntp.Query("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		t.Fatalf("Error while getting time from ntp: %v", err)
+	}
+	now := time.Now().Add(response.ClockOffset)
+	if response.Time.UTC().Sub(now.UTC()) > time.Microsecond {
+		t.Fatalf("Expected %v, got %v", now, response.Time)
+	}
+}
