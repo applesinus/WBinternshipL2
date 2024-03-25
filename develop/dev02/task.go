9ea91c120b5f9d09fafe546e1ec75a9cefad6a66
applesinus PC <dmitryapelsin4ik@gmail.com>
2024-03-25 10:17:52 +0300

    [ADD] task 2 + tests

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -18,6 +18,73 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// УТОЧНЕНИЕ НА СДАЧУ:
+// в тз не содержится информации про обработку прописных букв
+// тем не менее есть закомменченая строка с их обработкой
 
+import (
+	"strings"
+)
+
+// writeFinal writes character count times to given builder
+func writeFinal(final *strings.Builder, c rune, count int) {
+	final.Grow(count)
+	for i := 0; i < count; i++ {
+		final.WriteRune(c)
+	}
+}
+
+func unpack(str string) string {
+	// declaring variables
+	var final strings.Builder
+	prev := '-'
+	escaped := false
+
+	for _, c := range str {
+		switch {
+
+		// if a character is a back slash
+		case c == '\\':
+			if escaped {
+				// if it's escaped, write it one time to final and set prev to it
+				writeFinal(&final, c, 1)
+				prev = c
+			}
+			escaped = !escaped
+
+		// if a character is a digit
+		case int(c) >= '0' && int(c) <= '9':
+			if escaped {
+				// if it's escaped, write it one time to final and set prev to it
+				writeFinal(&final, c, 1)
+				prev = c
+				// also set escaped to false
+				escaped = false
+			} else {
+				if prev == '-' {
+					// if prev is an invalid character, return an empty string
+					return ""
+				}
+				// if prev is a valid character, write it count-1 times to final
+				// count-1 because one time it's already written
+				writeFinal(&final, prev, int(c)-'0'-1)
+				// set prev to - to indicate it's not a valid character to write or use again
+				prev = '-'
+			}
+
+		// if a character is a letter
+		// use below line to process ALL CHARACTERS:
+		// case (int(c) >= 'a' && int(c) <= 'z') || (int(c) >= 'A' && int(c) <= 'Z'):
+		case int(c) >= 'a' && int(c) <= 'z':
+			if escaped {
+				// if it's escaped, return an empty string
+				return ""
+			}
+			// if prev is a valid character, write it one time to final  and set prev to it
+			writeFinal(&final, c, 1)
+			prev = c
+		}
+	}
+
+	return final.String()
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,75 @@
 package main
+
+import "testing"
+
+// given test 1
+// - "a4bc2d5e" => "aaaabccddddde"
+func Test1(t *testing.T) {
+	str := unpack("a4bc2d5e")
+	if str != "aaaabccddddde" {
+		t.Fatalf("Test 1: a4bc2d5e\nExpected %s, got %s", "aaaabccddddde", str)
+	}
+}
+
+// given test 2
+// - "abcd" => "abcd"
+func Test2(t *testing.T) {
+	str := unpack("abcd")
+	if str != "abcd" {
+		t.Fatalf("Test 2: abcd\nExpected %s, got %s", "abcd", str)
+	}
+}
+
+// given test 3
+// - "45" => ""
+func Test3(t *testing.T) {
+	str := unpack("45")
+	if str != "" {
+		t.Fatalf("Test 3: 45\nExpected %s (empty string), got %s", "", str)
+	}
+}
+
+// given test 4
+// - "" => ""
+func Test4(t *testing.T) {
+	str := unpack("")
+	if str != "" {
+		t.Fatalf("Test 4: (empty string)\nExpected %s (empty string), got %s", "", str)
+	}
+}
+
+// given test 5
+// - "qwe\4\5" => "qwe45"
+func Test5(t *testing.T) {
+	str := unpack("qwe\\4\\5")
+	if str != "qwe45" {
+		t.Fatalf("Test 5: qwe\\4\\5\nExpected %s, got %s", "qwe45", str)
+	}
+}
+
+// given test 6
+// - "qwe\45" => "qwe44444"
+func Test6(t *testing.T) {
+	str := unpack("qwe\\45")
+	if str != "qwe44444" {
+		t.Fatalf("Test 6: qwe\\45\nExpected %s, got %s", "qwe44444", str)
+	}
+}
+
+// given test 7
+// - "qwe\5" => "qwe\\\\"
+func Test7(t *testing.T) {
+	str := unpack("qwe\\\\5")
+	if str != "qwe\\\\\\\\\\" {
+		t.Fatalf("Test 7: qwe\\\\5\nExpected %s, got %s", "qwe\\\\\\\\\\", str)
+	}
+}
+
+// custom test to ensure that escaped 'a'-'z' are considered as a wrong expression
+// - "a\\b" => ""
+func Test8(t *testing.T) {
+	str := unpack("a\\b")
+	if str != "" {
+		t.Fatalf("Test 8: a\\b\nExpected %s (empty string), got %s", "", str)
+	}
+}
